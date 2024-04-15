20342265f574cf1920b878ed5660e4952ae81143
Domza64 <juricicdominik@protonmail.com>
2024-04-15 19:49:39 +0200

    Remove admin page from webspace

--- public/assets/js/Constants.js
+++ public/assets/js/Constants.js
@@ -4,10 +4,3 @@ export const GUEST_BOOK_SUBMIT_URL =
   "http://localhost:8081/domza/submitGuestBookComment";
 export const GUEST_BOOK_COMMENTS_URL =
   "http://localhost:8081/domza/guestBookComments";
-export const CLIENT_ID = "domza-account-public-client";
-export const TOKEN_ENDPOINT =
-  "https://accounts.domza.xyz/realms/domza-account-dev/protocol/openid-connect/token";
-export const AUTHORIZATION_ENDPOINT =
-  "https://accounts.domza.xyz/realms/domza-account-dev/protocol/openid-connect/auth";
-export const UPDATE_ARTICLE_URL =
-  "http://localhost:8081/domza/admin/updateDiaryArticle";
--- public/assets/js/ProdConstants.js
+++ public/assets/js/ProdConstants.js
@@ -4,10 +4,3 @@ export const GUEST_BOOK_SUBMIT_URL =
   "https://domza-api.domza.xyz/domza/submitGuestBookComment";
 export const GUEST_BOOK_COMMENTS_URL =
   "https://domza-api.domza.xyz/domza/guestBookComments";
-export const CLIENT_ID = "domza-account-public-client";
-export const TOKEN_ENDPOINT =
-  "https://accounts.domza.xyz/realms/domza-account-dev/protocol/openid-connect/token";
-export const AUTHORIZATION_ENDPOINT =
-  "https://accounts.domza.xyz/realms/domza-account-dev/protocol/openid-connect/auth";
-export const UPDATE_ARTICLE_URL =
-  "https://domza-api.domza.xyz/domza/admin/updateDiaryArticle";
