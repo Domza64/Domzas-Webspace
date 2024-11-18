f097a2b0213e1f5f07a55972792b055501539a89
Domza64 <juricicdominik@protonmail.com>
2024-11-18 16:04:26 +0100

    Remove old files | Add css for diary page

--- OLD/public/assets/js/Constants.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = "http://localhost:8081/domza/";
-export const GUEST_BOOK_SUBMIT_URL =
-  "http://localhost:8081/domza/submitGuestBookComment";
-export const GUEST_BOOK_COMMENTS_URL =
-  "http://localhost:8081/domza/guestBookComments";
--- OLD/public/assets/js/ProdConstants.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = "https://domza-api.domza.xyz/domza/";
-export const GUEST_BOOK_SUBMIT_URL =
-  "https://domza-api.domza.xyz/domza/submitGuestBookComment";
-export const GUEST_BOOK_COMMENTS_URL =
-  "https://domza-api.domza.xyz/domza/guestBookComments";
